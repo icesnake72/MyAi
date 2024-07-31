80f202a28587efa5d33af3ecf8bae98b63c01bc7
Man.날.코딩 <icesnake72@gmail.com>
2024-07-31 18:56:44 +0900

    first commit

--- app/src/main/java/com/example/myai/MainActivity.java
+++ app/src/main/java/com/example/myai/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.myai;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +23,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.facebook.common.executors.CallerThreadExecutor;
 import com.facebook.common.executors.ConstrainedExecutorService;
@@ -41,13 +51,14 @@ import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editUrl;
-    private ImageView btnSearch;
+    private static final int REQUEST_PICK_IMAGE = 1000;
+    private ImageButton btnImageSearch;
     private ImageView imageViewSource;
     private TextView textViewInfo;
 //    private SimpleDraweeView draweeView;
@@ -69,25 +80,42 @@ public class MainActivity extends AppCompatActivity {
 
         imageViewSource = findViewById(R.id.imageViewSource);
         textViewInfo = findViewById(R.id.textViewInfo);
-        editUrl = findViewById(R.id.editUrl);
-        btnSearch = findViewById(R.id.imageViewSearch);
-        btnSearch.setOnClickListener(this::onSearchButtonClick);
+        btnImageSearch = findViewById(R.id.imageButtonSearch);
+        btnImageSearch.setOnClickListener(this::onSearchButtonClick);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(this.getClass().getSimpleName(), "Request Permission 결과 : " + permissions[0] + ", " + grantResults[0]);
     }
 
     private void onSearchButtonClick(View view)
     {
-        String url = editUrl.getText().toString();
-        editUrl.setText("");
+//        String url = editUrl.getText().toString();
+//        editUrl.setText("");
 
-        if (url.isEmpty()) {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                                         "이미지 URL을 입력하세요",
-                                         Toast.LENGTH_SHORT);
-            toast.show();
-            return;
-        }
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");  //          /storage/emulated/0/Pictures
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
 
-        Uri imageUri = Uri.parse(url);
+
+
+//        if (url.isEmpty()) {
+//            Toast toast = Toast.makeText(getApplicationContext(),
+//                                         "이미지 URL을 입력하세요",
+//                                         Toast.LENGTH_SHORT);
+//            toast.show();
+//            return;
+//        }
+
+//        Uri imageUri = Uri.parse(url);
 
 //        DraweeController controller = Fresco.newDraweeControllerBuilder()
 //                .setUri(imageUri)
@@ -97,26 +125,26 @@ public class MainActivity extends AppCompatActivity {
 //                .setControllerListener(listener)
 //                .build();
 
-        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(imageUri)
-                .build();
-
-        DataSource<CloseableReference<CloseableImage>> dataSource =
-                    Fresco.getImagePipeline().fetchDecodedImage(request, getApplicationContext());
-
-        dataSource.subscribe(new BaseBitmapDataSubscriber() {
-                                 @Override
-                                 public void onNewResultImpl(@Nullable Bitmap bitmap) {
-                                     // You can use the bitmap here, but in limited ways.
-                                     // No need to do any cleanup.
-                                     AnalyzeBitmap(bitmap);
-                                     imageViewSource.setImageBitmap(bitmap);
-                                 }
-
-                                 @Override
-                                 public void onFailureImpl(DataSource dataSource) {
-                                     // No cleanup required here.
-                                 }
-                             },  CallerThreadExecutor.getInstance());
+//        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(imageUri)
+//                .build();
+//
+//        DataSource<CloseableReference<CloseableImage>> dataSource =
+//                    Fresco.getImagePipeline().fetchDecodedImage(request, getApplicationContext());
+//
+//        dataSource.subscribe(new BaseBitmapDataSubscriber() {
+//                                 @Override
+//                                 public void onNewResultImpl(@Nullable Bitmap bitmap) {
+//                                     // You can use the bitmap here, but in limited ways.
+//                                     // No need to do any cleanup.
+//                                     AnalyzeBitmap(bitmap);
+//                                     imageViewSource.setImageBitmap(bitmap);
+//                                 }
+//
+//                                 @Override
+//                                 public void onFailureImpl(DataSource dataSource) {
+//                                     // No cleanup required here.
+//                                 }
+//                             },  CallerThreadExecutor.getInstance());
 
 //        DataSource<CloseableReference<CloseableImage>> dataSource =
 //                Fresco.getImagePipeline().fetchDecodedImage(request, getApplicationContext());
@@ -131,6 +159,37 @@ public class MainActivity extends AppCompatActivity {
 //            dataSource.close();
 //            CloseableReference.closeSafely((CloseableReference<?>) closeableImage);
 //        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( resultCode == RESULT_OK ) {
+            if ( requestCode==REQUEST_PICK_IMAGE ) {
+                Uri uri = data.getData();
+                Bitmap bitmap = loadImage(uri);
+                imageViewSource.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private Bitmap loadImage(Uri uri)
+    {
+//        Log.d(getClass().getSimpleName(), "PATH : " + imageUrl);
+        Bitmap bitmap = null;
+
+        try {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O_MR1) {
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            } else {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
     }
 
 
